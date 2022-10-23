43adfb1bf6c89443c36d9f2c1cf5dd9bf9741d2e
AmitGujar <amithero3342@gmail.com>
2022-10-23 22:23:56 +0530

    docker added

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs")
-
+require("dotenv").config();
 
 app.set("view engine", "ejs")
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server started on 3000");
 })
 
 app.get("/", (req, res) => {
     res.render("index")
 })
+
